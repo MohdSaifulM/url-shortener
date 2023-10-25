48f98524c051abb474a11292833c28593bfe0033
SaifM <mohd.saiful@live.com>
2023-10-26 03:03:53 +0800

    ci: 🎡 Added deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Navigate to project directory
+cd url-shortener/
+
+# Stash any changes on ec2 repo
+git stash
+
+# Pull the latest code from repository
+git pull
+
+# Build your application (if needed)
+npm install          # Install dependencies
+npm run build        # Build your app (if necessary)
+
+# Reload your PM2-managed application
+pm2 reload server 
