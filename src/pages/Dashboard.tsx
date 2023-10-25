968e76e30a86d46d2686e61c60f255ae88b9dfc6
SaifM <mohd.saiful@live.com>
2023-10-26 02:37:53 +0800

    feat: 🎸 Dashboard copy to clipboard func

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import { Table, Tooltip } from "flowbite-react";
 import analytics_icon from "../assets/view_analytics.svg";
 import edit_url_icon from "../assets/url_edit.svg";
 import delete_url_icon from "../assets/url_delete.svg";
+import copy_url_icon from "../assets/copy.svg";
 
 function Dashboard() {
+
+    const [copied, setCopied] = useState(false);
 
     const tableHeaders = [
         "title",
@@ -32,7 +36,7 @@ function Dashboard() {
                 src: analytics_icon,
                 alt: "view analytics icon"
             }
-        }, 
+        },
         {
             tooltip_content: "Edit",
             img: {
@@ -48,6 +52,21 @@ function Dashboard() {
             }
         }
     ]
+
+    const copyToClipboard = (text: string) => {
+        navigator.clipboard.writeText(text)
+            .then(() => {
+                console.log('Text copied to clipboard');
+                // Show that url is copied and revert after 5 seconds
+                setCopied(true);
+                setTimeout(() => {
+                    setCopied(false);
+                }, 5000)
+            })
+            .catch((err) => {
+                console.error('Unable to copy text: ', err);
+            });
+    }
 
     return (
         <div className="m-10">
@@ -65,7 +84,20 @@ function Dashboard() {
                             <Table.Row key={url._id} className="bg-white dark:border-gray-700 dark:bg-gray-800">
                                 <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">{url.title}</Table.Cell>
                                 <Table.Cell>{url.original_url}</Table.Cell>
-                                <Table.Cell>{url.short_url}</Table.Cell>
+                                <Table.Cell className="flex gap-3 items-center">
+                                    {url.short_url}
+                                    <Tooltip
+                                        animation="duration-1000"
+                                        content={copied ? "Copied" : "Copy URL"}
+                                    >
+                                        <img
+                                            src={copy_url_icon}
+                                            className="mr-3 h-2 sm:h-4 cursor-pointer"
+                                            alt="copy url icon"
+                                            onClick={() => copyToClipboard(url.short_url)}
+                                        />
+                                    </Tooltip>
+                                </Table.Cell>
                                 <Table.Cell>{url.domain}</Table.Cell>
                                 <Table.Cell>{url.clicks}</Table.Cell>
                                 <Table.Cell className="flex">
